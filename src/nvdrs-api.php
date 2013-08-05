6ee61aec270fc9ea0b4f8cc02c6a17e6d8daa1b2
idiosyncraticee <idiosyncraticee@comcast.net>
2013-08-04 20:37:28 -0700

    All parameters available

--- src/nvdrs-api.php
+++ src/nvdrs-api.php
@@ -6,57 +6,84 @@ class WisqarsApi {
 	
 	public $dataArray = [];
 	protected $debugFlag = 1;
-	//Alaska, Colorado, Georgia, Kentucky, Maryland, Massachusetts, New Jersey, New Mexico, North Carolina, Oklahoma, Oregon, Rhode Island, South Carolina, Utah, Virginia, Wisconsin
-	protected $states = array("Oregon"=>"41","Alaska"=>"02","Oklahoma"=>"40");
-	//protected $states = array("Oregon"=>41);
-	//protected $years = array(2003,2004,2005,2006,2007,2008,2009,2010);
-	protected $years = array(2010);
+	protected $useCache = 1;
 	
-	protected $ages = array("15-19"=>3);
+	//LIST OF STATES FOR THE National Violent Death Reporting System
+	protected $states = array(
+// 			"Alaska"=>"02",
+// 			"Colorado"=>"08",
+// 			"Georgia"=>"13",
+// 			"Kentucky"=>"21",
+// 			"Maryland"=>"24",
+// 			"Massachusetts"=>"25",
+// 			"New Jersey"=>"34",
+// 			"New Mexico"=>"35",
+// 			"North Carolina"=>"37",
+// 			"Oklahoma"=>"40",
+ 			"Oregon"=>"41",
+// 			"Rhode Island"=>"44",
+// 			"South Carolina"=>"45",
+// 			"Utah"=>"49",
+// 			"Virginia"=>"51",
+// 			"Wisconsin"=>"55"
+	);
+	
+	//LIST OF YEARS WITH SOME AMOUNT OF DATA.  NOTICE THAT THERE ARE MORE YEARS AVAILABLE THEN THE FRONT END LETS ON
+	protected $years = array(2003,2004,2005,2006,2007,2008,2009,2010);
+
+	//LIST OF AGES AVAILABLE
+	//TODO: THIS CAN ALSO BE A SPECIFIC RANGE FOR BETTER RESOLUTION
+	protected $ages = array(
+			"10-14"=>2,
+			"15-19"=>3,
+			"20-24"=>4,
+			"25-29"=>5,
+			"30-34"=>6,
+			"35-39"=>7,
+			"40-44"=>8,
+			"45-49"=>9,
+			"50-54"=>10,
+			"55-59"=>11,
+			"60-64"=>12,
+			"65-69"=>13,
+			"70-74"=>14,
+			"75-79"=>15,
+			"80-84"=>16,
+			"85-89"=>17,
+			"Unknown"=>18
+	);
+
+
+	
 	
 	public function __construct() {
 		//$this->data_array = (array) $arg1;
-		//$this->desiredVenuesArray = $arg2;
 
 	}
 	
-// 	<select name="q7s2i1">
-// 	<option value="0" selected>0-4</option>
-// 	<option value="1">5-9</option>
-// 	<option value="2">10-14</option>
-// 	<option value="3">15-19</option>
-// 	<option value="4">20-24</option>
-// 	<option value="5">25-29</option>
-// 	<option value="6">30-34</option>
-// 	<option value="7">35-39</option>
-// 	<option value="8">40-44</option>
-// 	<option value="9">45-49</option>
-// 	<option value="10">50-54</option>
-// 	<option value="11">55-59</option>
-// 	<option value="12">60-64</option>
-// 	<option value="13">65-69</option>
-// 	<option value="14">70-74</option>
-// 	<option value="15">75-79</option>
-// 	<option value="16">80-84</option>
-// 	<option value="17">85+</option>
-// 	<option value="18">Unknown</option>
+	protected function _cacheName() {
 
-// 	States[0] = new Array( "Alaska","02");
-// 	States[1] = new Array( "Colorado","08");
-// 	States[2] = new Array( "Georgia","13");
-// 	States[3] = new Array( "Kentucky","21");
-// 	States[4] = new Array( "Maryland","24");
-// 	States[5] = new Array( "Massachusetts","25");
-// 	States[6] = new Array( "New Jersey","34");
-// 	States[7] = new Array( "New Mexico","35");
-// 	States[8] = new Array( "North Carolina","37");
-// 	States[9] = new Array( "Oklahoma","40");
-// 	States[10] = new Array( "Oregon","41");
-// 	States[11] = new Array( "Rhode Island","44");
-// 	States[12] = new Array( "South Carolina","45");
-// 	States[13] = new Array( "Utah","49");
-// 	States[14] = new Array( "Virginia","51");
-// 	States[15] = new Array( "Wisconsin","55");
+		$filename = "../caches/cache.txt";
+		return $filename;
+	}
+	
+	protected function _writeCache() {
+	
+		file_put_contents($this->_cacheName(), json_encode($this->dataArray));
+	}
+
+	protected function _getMatchingFromCache() {
+	
+		#DOES THE CACHE FILE EXIST?
+		if (file_exists($this->_cacheName())) {
+			$this->dataArray = json_decode(file_get_contents($this->_cacheName() ), true);
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+
+
 		
 	public function getData() {
 		
@@ -66,10 +93,13 @@ class WisqarsApi {
 				foreach($this->ages as $age => $ageId) {
 			
 					$deathCount = $this->getDeathCount($year, $stateId, $ageId);
-					print "DEATH COUNT IN $state ($stateId) IN THE YEAR $year = ${deathCount} FOR $age ($ageId)<br>";
+					print "COUNT IN $state ($stateId) IN THE YEAR $year = ${deathCount} FOR $age ($ageId)<br>";
+					$this->dataArray[$year][$state][$age]=$deathCount;
 				}
 			}
 		}
+		
+		$this->_writeCache();
 	}
 
 	protected function getDeathCount($year, $stateId, $ageId) {
@@ -131,7 +161,6 @@ class WisqarsApi {
 		curl_setopt($ch, CURLOPT_URL,$http_server);
 		curl_setopt($ch, CURLOPT_POST, 1);
 		curl_setopt($ch, CURLOPT_POSTFIELDS,$post_variables);
-		//curl_setopt($ch, CURLOPT_COOKIE,'JSESSIONID=69F9C9AAD5540F06CF7A96E879D77516; s_vi=[CS]v1|28FF59A18514A734-60000180C0264F2C[CE]; fsr.r.session.11.2.0=%7B%22d%22%3A30%2C%22i%22%3A%22d036702-53533301-568d-89d8-ce3e3%22%2C%22e%22%3A1376252923353%7D; s_ria=Flash%2011%7C; ForeseeLoyalty_MID_soL8o3iA4g=3; s_cc=true; s_sq=; s_ppv=56; fsr.s.session.11.2.0={"v":1,"rid":"d036702-53533301-568d-89d8-ce3e3","ru":"http://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&ved=0CC0QFjAA&url=http://www.cdc.gov/nchs/fastats/suicide.htm&ei=irL-Ue27HYqyiQKaooGABA&usg=AFQjCNFCx5q5TtoDKofLGBFHN2NtV0C-tA&sig2=APpYrqexQ5neDwUzJhlZuA&bvm=bv.50165853,d.cGE","r":"www.google.com","st":"","cp":{"GovDelivery":"N","Injury":"Y"},"to":5,"c":"http://www.cdc.gov/violenceprevention/suicide/datasources.html","pv":15,"lc":{"d0":{"v":15,"s":true}},"cd":0,"f":1375653402658,"sd":0,"i":1,"l":"en","s":1}');
 	
 		$contents = curl_exec($ch);
 		curl_close($ch);
@@ -148,7 +177,7 @@ class WisqarsApi {
 		foreach($table->find('tr') as $row) {
 
 			if($rowCounter==1){
-				$deathCount = $row->find('td',1)->plaintext;
+				$deathCount = trim($row->find('td',1)->plaintext);
 			}
 			
 // 			foreach($row->find('td') as $cell) {
@@ -160,21 +189,6 @@ class WisqarsApi {
 
 		return $deathCount;
 		
-// 		$obj = json_decode($contents, true);
-// 		var_dump($contents);
-		
-// 		if ($contents != "OK") {
-// 	      echo "SOMETHING WENT WRONG IN THE QUERY AND THE STATUS IS ".$obj['resultsPage']['error']['message']."<br>\n";
-// 		      		if($this->debugFlag) {
-// 		      				echo "THE ISSUED QUERY IS: $http_server";
-// 		      				echo "THE ISSUED PARAMTERS ARE: $post_variables";
-// 		      				var_dump($obj);
-// 		      				}
-// 		      				die;
-// 		      				return 0;
-// 		} else {
-// 			return 1;
-// 		}
 	}
 	
 }
